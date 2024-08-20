3eb3b33f96cca522cd511080f91c5360850c9f76
YC00 <q36007068@mail.ncku.edu.tw>
2024-08-21 01:03:27 +0800

    cors

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		origin: 'https://ai-handwriting-ocr.p.rapidapi.com',
+		host: 'rapidapi.com',
+		port: 80
+	}
 });
